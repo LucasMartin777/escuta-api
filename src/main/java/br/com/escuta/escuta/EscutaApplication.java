381e0823b10491dbd3af01c1a99ed327b932f987
Lucas Martin Bogner Chiquito <lucaschiquito2@hotmail.com>
2025-11-19 16:49:39 -0300

    feat: inicialização do projeto Escuta
    - Criação da estrutura base do projeto com Spring Boot 3 (Java 21)
    - Configuração inicial do Maven e dependências essenciais
    - Criação da hierarquia de pastas do backend

---
+++ src/main/java/br/com/escuta/escuta/EscutaApplication.java
@@ -0,0 +1,13 @@
+package br.com.escuta.escuta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscutaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscutaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/escuta/escuta/EscutaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escuta.escuta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscutaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
